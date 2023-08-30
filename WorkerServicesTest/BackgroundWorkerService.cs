4d58c0a2fa0d25e9639d02aca3d78d2690daa4dc
Md Nazim Uddin <nazim@trustaxiatapay.com>
2023-08-30 13:54:06 +0600

    Sample Project for background sevice in asp.net core

---
+++ WorkerServicesTest/BackgroundWorkerService.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Threading;
+
+public class BackgroundWorkerService : BackgroundService
+{
+    readonly ILogger<BackgroundWorkerService> _logger;
+
+    public BackgroundWorkerService(ILogger<BackgroundWorkerService> logger)
+    {
+            _logger = logger;
+    }
+    //public async Task StartAsync(CancellationToken cancellationToken)
+    //{
+    //    _logger.LogInformation("Service Started");
+    //}
+
+    //public Task StopAsync(CancellationToken cancellationToken)
+    //{
+    //    _logger.LogInformation("Service Stopped");
+    //    return Task.CompletedTask;
+    //}
+
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker Service Started at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
